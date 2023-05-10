9ce15094f8066bd456f6a25e02d3b8758983d229
Tsimur Mikula <mikulatsimur@yandex.ru>
2023-05-10 19:25:41 +0300

    feat: adjustment of tasks 2 and 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const promise = require('fs/promises');
 
 fs.mkdir(path.join(__dirname, 'project-dist'), { recursive: true }, (err) => {
   if (err) {
@@ -13,115 +14,50 @@ fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), { recursive: true }, (e
   }
 });  
 
-function copyAssets () {
-  fs.readdir(path.join(__dirname, 'assets'), (err, dir) => {
-    if (err) {
-      console.log('Ошибка!');
-    } else {
-      for (let i = 0; i < dir.length; i++) {
-        fs.mkdir(path.join(__dirname, 'project-dist', 'assets', `${dir[i]}`), { recursive: true }, (err) => {
-          if (err) {
-            console.log('Папка не создана!');
-          }
-        });
-        fs.readdir(path.join(__dirname, 'project-dist', 'assets', `${dir[i]}`), (err, file) => {
-          if (err) {
-            console.log('Ошибка!');
-          } else {
-            for (let j = 0; j < file.length; j++) {
-              fs.unlink(path.join(__dirname, 'project-dist', 'assets', `${dir[i]}`, `${file[j]}`), (err) => {
-                if (err) {
-                  console.log('Ошибка!');
-                }
-              });
-            }
-          }  
-        });  
-        fs.readdir(path.join(__dirname, 'assets', `${dir[i]}`), (err, file) => {
-          if (err) {
-            console.log('Ошибка!');
-          } else {
-            for (let j = 0; j < file.length; j++) {
-              fs.copyFile(path.join(__dirname, 'assets', `${dir[i]}`, `${file[j]}`), path.join(__dirname, 'project-dist', 'assets', `${dir[i]}`, `${file[j]}`), (err) => {
-                if (err) {
-                  console.log('Ошибка!');
-                }
-              });
-            }
-          }  
-        });  
-      }
+async function copyAssets () {
+  let assetsRoot = await promise.readdir(path.join(__dirname, 'assets'));
+  for (const asset of assetsRoot) {
+    await promise.mkdir(path.join(__dirname, 'project-dist', 'assets', `${asset}`), { recursive: true });
+    let assetCopy = await promise.readdir(path.join(__dirname, 'project-dist', 'assets', `${asset}`));
+    for (const file of assetCopy) {
+      await promise.unlink(path.join(__dirname, 'project-dist', 'assets', `${asset}`, `${file}`));
     }
-  });
+    let assetRoot = await promise.readdir(path.join(__dirname, 'assets', `${asset}`));
+    for (const file of assetRoot) {
+      await promise.copyFile(path.join(__dirname, 'assets', `${asset}`, `${file}`), path.join(__dirname, 'project-dist', 'assets', `${asset}`, `${file}`));  
+    }
+  }
 }  
 copyAssets();
 
-function createHtml () {
-  fs.copyFile(path.join(__dirname, 'template.html'), path.join(__dirname, 'project-dist', 'index.html'), (err) => {
-    if (err) {
-      console.log('Ошибка!');
-    } else {
-      fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, templ) =>  {
-        if (err) {
-          console.log('Ошибка!');
-        } else {
-          fs.readdir(path.join(__dirname, 'components'), {withFileTypes: true}, (err, files) => {
-            if (err) {
-              console.log('Ошибка!');
-            } else {
-              for (let i = 0; i < files.length; i++) {
-                if (files[i].isFile() && path.extname(files[i].name) === '.html') {
-                  fs.readFile(path.join(__dirname, 'components', `${files[i].name}`), 'utf-8', (err, comp) =>  {
-                    if (err) {
-                      console.log('Ошибка!');
-                    } else {
-                      templ = templ.replace(`{{${files[i].name.split('.').slice(0, -1).join('')}}}`, comp);
-                      fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), templ, (err) => {
-                        if (err) {
-                          console.log('Ошибка!');
-                        }
-                      });
-                    }
-                  });
-                }
-              }
-            }  
-          }); 
-        }       
-      });    
-    }
-  });
+async function createHtml () {
+  let template = await promise.readFile(path.join(__dirname, 'template.html'), 'utf-8');
+  let components = await promise.readdir(path.join(__dirname, 'components'), {withFileTypes: true});
+  for (const comp of components) {
+    if (comp.isFile() && path.extname(comp.name) === '.html') {
+      let file = await promise.readFile(path.join(__dirname, 'components', `${comp.name}`), 'utf-8');
+      template = template.replace(`{{${comp.name.split('.').slice(0, -1).join('')}}}`, file);
+    }   
+  }
+ await promise.writeFile(path.join(__dirname, 'project-dist', 'index.html'), template);
 }
 createHtml();
 
-function createCss () {
-  fs.writeFile(path.join(__dirname, 'project-dist', 'style.css'), '', (err) => {
-    if (err) {
-      console.log('Ошибка!');
-    }
-  });
 
-  fs.readdir(path.join(__dirname, 'styles'), {withFileTypes: true}, (err, files) => {
-    if (err) {
-      console.log('Ошибка!');
-    } else {
-      for (let i = 0; i < files.length; i++) {
-        if (files[i].isFile() && path.extname(files[i].name) === '.css') {
-          fs.readFile(path.join(__dirname, 'styles', `${files[i].name}`), 'utf-8', (err, data) =>  {
-            if (err) {
-              console.log('Ошибка!');
-            } else {
-              fs.appendFile(path.join(__dirname, 'project-dist', 'style.css'), data, (err) => {
-                if (err) {
-                  console.log('Ошибка!');
-                }
-              });
-            }       
-          });
+async function createCss () {
+  await promise.writeFile(path.join(__dirname, 'project-dist', 'style.css'), '',);
+
+  let styles = await promise.readdir(path.join(__dirname, 'styles'), {withFileTypes: true});
+  for (const style of styles) {
+    if (style.isFile() && path.extname(style.name) === '.css') {
+      let appendStyle = await promise.readFile(path.join(__dirname, 'styles', `${style.name}`), 'utf-8')
+      await promise.appendFile(path.join(__dirname, 'project-dist', 'style.css'), appendStyle, (err) => {
+        if (err) {
+          console.log('Ошибка!');
         }
-      }
-    }  
-  });
+      });
+    }
+  }
 }
 createCss();
 
